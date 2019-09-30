77d27b07037d77e07f049d27a203dde41653f643
yushilong <1074985929@qq.com>
2019-09-30 17:30:10 +0800

    模块化小作业

---
+++ test/1.module.js
@@ -0,0 +1,9 @@
+function add (a, b){
+    return a+b;
+}
+function mul (a, b){
+    return a*b;
+}
+
+exports.add = add;
+exports.mul = mul;
---
+++ test/2.module.js
@@ -0,0 +1,3 @@
+var one = require('./1.module.js');
+console.log(one.add(5, 6));
+console.log(one.mul(5, 6));
